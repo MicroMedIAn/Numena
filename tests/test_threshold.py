4621fc4626a1b9a4b3897f3654c31816a8bc1c90
Kévin Cortacero <kevin.cortacero@inserm.fr>
2022-07-28 19:51:35 +0200

    test file

---
+++ tests/test_threshold.py
@@ -0,0 +1,17 @@
+import unittest
+
+from numen.image.basics import image_new
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.image = image_new((16, 16))
+        self.image[0:8, :] = 255
+        self.image[8:, :] = 128
+
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == "__main__":
+    unittest.main()
